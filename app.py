4b367f9285fb2ff2b7e480ae3cf989a8ffd3eb9e
Ron Hernaus <ron.hernaus@rws.nl>
2023-05-08 20:34:01 +0200

    formatting

--- app.py
+++ app.py
@@ -1,166 +1,129 @@
 import openai
 import os
-from dotenv import load_dotenv
 import streamlit as st
+from dotenv import load_dotenv
 
 load_dotenv(verbose=True, override=True)
-
-# Set up the OpenAI API client
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-
-# Define the list of Subject Matter Experts
-smes = [
+SUBJECT_MATTER_EXPERTS = [
     # Natural Sciences
-    "Physicist",
-    "Chemist",
-    "Biologist",
-    "Astronomer",
-    "Earth Scientist",
-
+    "Physicist", "Chemist", "Biologist", "Astronomer", "Earth Scientist",
     # Social Sciences
-    "Sociologist",
-    "Psychologist",
-    "Anthropologist",
-    "Economist",
-    "Political Scientist",
-
+    "Sociologist", "Psychologist", "Anthropologist", "Economist", "Political Scientist",
     # Humanities
-    "Philosopher",
-    "Historian",
-    "Literary Scholar",
-    "Linguist",
-    "Religious Studies Scholar",
-
+    "Philosopher", "Historian", "Literary Scholar", "Linguist", "Religious Studies Scholar",
     # Formal Sciences
-    "Mathematician",
-    "Logician",
-    "Statistician",
-    "Computer Scientist",
-
+    "Mathematician", "Logician", "Statistician", "Computer Scientist",
     # Applied Sciences
-    "Engineer",
-    "Medical Doctor",
-    "Architect",
-    "Agricultural Scientist",
-
+    "Engineer", "Medical Doctor", "Architect", "Agricultural Scientist",
     # Arts
-    "Visual Artist",
-    "Performing Artist",
-    "Musician",
-    "Filmmaker",
-
+    "Visual Artist", "Performing Artist", "Musician", "Filmmaker",
     # Sports and Recreation
-    "Sports Coach",
-    "Fitness Trainer",
-    "Recreational Activities Expert",
-
+    "Sports Coach", "Fitness Trainer", "Recreational Activities Expert",
     # Law
-    "Legal Expert",
-    "Constitutional Law Expert",
-    "Criminal Law Expert",
-    "International Law Expert",
-
+    "Legal Expert", "Constitutional Law Expert", "Criminal Law Expert", "International Law Expert",
     # Business and Finance
-    "Management Expert",
-    "Finance Expert",
-    "Marketing Expert",
-    "Human Resources Expert",
-
+    "Management Expert", "Finance Expert", "Marketing Expert", "Human Resources Expert",
     # Education
-    "Pedagogy Expert",
-    "Educational Theory Expert",
-    "Learning Methodologies Expert",
-
+    "Pedagogy Expert", "Educational Theory Expert", "Learning Methodologies Expert",
     # Languages
     "Language Expert",
-
     # Communications
-    "Media Expert",
-    "Journalist",
-    "Public Relations Expert",
-
+    "Media Expert", "Journalist", "Public Relations Expert",
     # Technology
-    "Information Technology Expert",
-    "Artificial Intelligence Expert",
-    "Robotics Expert",
-    "Nanotechnology Expert",
-
+    "Information Technology Expert", "Artificial Intelligence Expert", "Robotics Expert", "Nanotechnology Expert",
     # Environment
-    "Ecologist",
-    "Environmental Scientist",
-    "Conservation Expert",
+    "Ecologist", "Environmental Scientist", "Conservation Expert",
 ]
 
 
-def classify_question(question):
-    prompt = f"Classify the following question and select the top 3 most relevant Subject Matter Experts from the list: {question}\n\n{', '.join(smes)}\n\nOnly answer with relevant Subject Matter Experts. No explanation is allowed.\n\nTop 3 SMEs:"
-    response = openai.ChatCompletion.create(
+def create_chat_completion(system_prompt, user_prompt):
+    """
+    Create a chat completion using OpenAI's GPT-4.
+
+    Args:
+        system_prompt (str): The system role prompt.
+        user_prompt (str): The user role prompt.
+
+    Returns:
+        openai.ChatCompletion: A ChatCompletion object containing the response.
+    """
+    return openai.ChatCompletion.create(
         model="gpt-4",
         messages=[
-            {"role": "system", "content": "You are a Project Manager."},
-            {"role": "user", "content": prompt},
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt},
         ],
         max_tokens=1000,
         n=1,
         stop=None,
         temperature=0.7,
     )
-    selected_smes = [sme.strip() for sme in response.choices[0].message["content"].strip().split(',')]
-    return selected_smes[:3]  # Return the top 3 SMEs
 
+
+def classify_question(question):
+    """
+    Classify the given question and select the top 3 most relevant Subject Matter Experts.
+
+    Args:
+        question (str): The question to classify.
+
+    Returns:
+        list: A list containing the top 3 most relevant Subject Matter Experts.
+    """
+    system_prompt = "You are a Project Manager."
+    user_prompt = f"Classify the following question and select the top 3 most relevant Subject Matter Experts from the list: {question}\n\n{', '.join(SUBJECT_MATTER_EXPERTS)}\n\nTop 3 SMEs:"
+
+    response = create_chat_completion(system_prompt, user_prompt)
+    return [sme.strip() for sme in response.choices[0].message["content"].strip().split(',')][:3]
 
 
 def consult_smes(question, selected_smes):
+    """
+    Consult the selected Subject Matter Experts and gather their answers.
+
+    Args:
+        question (str): The question to consult the SMEs about.
+        selected_smes (list): The list of selected Subject Matter Experts.
+
+    Returns:
+        dict: A dictionary containing the responses from the SMEs.
+    """
     responses = []
     for sme in selected_smes:
-        prompt = f"How would you answer this question: {question}? Let's work this out in a step by step way to be sure we have the right answer."
-        response = openai.ChatCompletion.create(
-            model="gpt-4",
-            messages=[
-                {"role": "system", "content": f"You are a {sme}."},
-                {"role": "user", "content": prompt},
-            ],
-            max_tokens=1000,
-            n=1,
-            stop=None,
-            temperature=0.7,
-        )
+        system_prompt = f"You are a {sme}."
+        user_prompt = f"How would you answer this question: {question}? Let's work this out in a step by step way to be sure we have the right answer."
+        response = create_chat_completion(system_prompt, user_prompt)
         responses.append(response.choices[0].message["content"].strip())
-    return responses
+        return {selected_smes[i]: response for i, response in enumerate(responses)}
 
 def resolve_best_answer(question, answers):
-    prompt = f"Given the question '{question}' and the following answers from various Subject Matter Experts, choose the best answer:\n\n"
-    for i, (sme, answer) in enumerate(answers.items()):
-        prompt += f"{i + 1}. {sme}: {answer}\n"
-    prompt += f"\nPrint the Subject Matter Expert and answer in full:"
+    """
+    Resolve the best answer from the provided options given by Subject Matter Experts.
+    Args:
+        question (str): The original question.
+        answers (dict): A dictionary containing the answers provided by SMEs.
 
-    response = openai.ChatCompletion.create(
-        model="gpt-4",
-            messages=[
-                {"role": "system", "content": "You are a resolver tasked with finding which of the answer options the Subject Matter Experts have provided is the best answer."},
-                {"role": "user", "content": prompt},
-            ],
-        max_tokens=1000,
-        n=1,
-        stop=None,
-        temperature=0.7,
-    )
+    Returns:
+        str: The best answer.
+    """
+    system_prompt = "You are a resolver tasked with finding which of the answer options the Subject Matter Experts have provided is the best answer."
+    user_prompt = f"Given the question '{question}' and the following answers:\n\n"
+    for i, (sme, answer) in enumerate(answers.items()):
+        user_prompt += f"{i + 1}. {sme}: {answer}\n"
+    user_prompt += f"\nBest answer:"
+    response = create_chat_completion(system_prompt, user_prompt)
     return response.choices[0].message["content"].strip()
 
-def project_manager(question):
-    selected_smes = classify_question(question)
-    sme_responses = consult_smes(question, selected_smes)
-    return {selected_smes[i]: response for i, response in enumerate(sme_responses)}
-
-
 def main():
+    """
+    Main function for the Project Manager with GPT-4 Streamlit app.
+    """
     st.set_page_config(page_title="Project Manager with GPT-4")
-
     st.image("img/logo.png")
     st.title("Project Manager with GPT-4")
     st.write("Ask a question, and get answers from Subject Matter Experts:")
-
     user_question = st.text_area("Enter your question:")
 
     if st.button("Get Answers"):
@@ -173,9 +136,7 @@ def main():
                 st.text(sme)
 
             with st.spinner("Getting answers from SMEs..."):
-                sme_responses = consult_smes(user_question, selected_smes)
-
-            answers = {selected_smes[i]: response for i, response in enumerate(sme_responses)}
+                answers = consult_smes(user_question, selected_smes)
 
             st.write("Responses from SMEs:")
             for sme, answer in answers.items():
@@ -186,8 +147,9 @@ def main():
 
             st.text_area("Best Answer", value=best_answer, height=50, disabled=True)
 
-        else:
-            st.warning("Please enter a question.")
+    else:
+        st.warning("Please enter a question.")
 
-if __name__ == "__main__":
+
+if __name__ == "main":
     main()
