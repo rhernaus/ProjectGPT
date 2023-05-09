2832b795f297de40261f96cb6ccc407c81839487
Ron Hernaus <ron.hernaus@rws.nl>
2023-05-09 22:51:18 +0200

    improvement in performance

--- projectgpt.py
+++ projectgpt.py
@@ -38,7 +38,6 @@ tools.append(
     )
 )
 
-
 def classify_question(question: str) -> List[str]:
     """
     Classify the given question and select the top 3 most relevant Subject Matter Experts.
@@ -49,9 +48,9 @@ def classify_question(question: str) -> List[str]:
     Returns:
         list: A list containing the top 3 most relevant Subject Matter Experts.
     """
-    prompt = "You are a Project Manager. Do not use any tools. Classify the following question and select the top 3 most relevant Subject Matter Experts. "
+    prompt = "You are a Project Manager. Do not use any tools. Classify the following question and select the top 1 most relevant Subject Matter Expert. "
     prompt += f"Question: {question}."
-    prompt += 'Respond with the top 3 SMEs in the following JSON template. Do NOT print anything else! {"smes": ["sme", "sme", "sme"]}: '
+    prompt += 'Respond with the top 1 SME in the following JSON template. Do NOT print anything else! {"smes": ["sme", "sme", "sme"]}: '
     response = llm([HumanMessage(content=prompt)]).content
     # Parse the JSON response into a list
     try:
@@ -75,10 +74,15 @@ def consult_smes(question: str, selected_smes: List[str]) -> Dict[str, str]:
     """
     responses = {}
     agent = initialize_agent(tools, llm, agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
-    for sme in selected_smes:
-        prompt = f"You are a {sme}. Let's work this out in a step by step way to be sure we have the right answer. You have to give a definitive answer to this question: {question}?"
+    #for sme in selected_smes:
+    sme = selected_smes[0]
+    prompt = f"You are a {sme}. Let's work this out in a step by step way to be sure we have the right answer. You have to give a definitive answer to this question: {question}?"
+    try:
         response = agent.run(input=prompt)
         responses[sme] = response
+    except Exception as e:
+        print(f"An error occurred while running the agent for {sme}. Error: {e}")
+        responses[sme] = "An error occurred while running the agent."
     return responses
 
 
@@ -93,7 +97,7 @@ def resolve_best_answer(question, answers):
         str: The best answer.
     """
     agent = initialize_agent(tools, llm, agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
-    prompt = "You are a resolver tasked with finding which of the answer options the Subject Matter Experts have provided is the best answer. Let's work this out in a step by step way to be sure we have the right answer.\n\n"
+    prompt = "You are a resolver tasked with finding which of the answer options the Subject Matter Experts have provided is the best answer. Let's work this out in a step by step way to be sure we have the right answer. Do not use tools.\n\n"
     prompt += f"Given the question '{question}' and the following answers:\n\n"
     for i, (sme, answer) in enumerate(answers.items()):
         prompt += f"{i + 1}. {sme}: {answer}\n"
